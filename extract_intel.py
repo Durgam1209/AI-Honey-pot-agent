0eb3e8cdb80574c985579a031373490d7efcd8e6
Durgam1209 <durga.m1209@gmail.com>
2026-02-05 19:36:26 +0530

    App update

--- extract_intel.py
+++ extract_intel.py
@@ -8,8 +8,9 @@ def extract_intel(text: str):
     url_pattern = r"https?://[^\s]+"
     phone_pattern = r"(?:\+?91[-\s]?)?[6-9]\d{9}\b"
 
+    upis = [u for u in set(re.findall(upi_pattern, text)) if "http" not in u.lower()]
     return {
-        "upi_ids": list(set(re.findall(upi_pattern, text))),
+        "upi_ids": upis,
         "bank_accounts": list(set(re.findall(bank_pattern, text))),
         "ifsc_codes": list(set(re.findall(ifsc_pattern, text))),
         "phishing_urls": list(set(re.findall(url_pattern, text))),
